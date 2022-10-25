4fea5dc885792ad72cba6cd6bcd41af5e5d6bdee
Musab Söylemez <musabsoylemez@hotmail.com>
2022-10-25 17:44:23 +0300

    Implemented Decorator Pattern

---
+++ DecoratorPattern/src/CondimentDecorator.java
@@ -0,0 +1,8 @@
+public abstract class CondimentDecorator extends Beverage {
+    Beverage beverage;
+
+    @Override
+    public String getDescription() {
+        return super.getDescription();
+    }
+}
