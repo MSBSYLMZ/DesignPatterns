31b449ab9cbdfeecf1ae2bdbd87060f67f1cbda3
Musab Söylemez <musabsoylemez@hotmail.com>
2022-10-26 11:08:14 +0300

    Implemented Factory Pattern

---
+++ FactoryPattern/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        PizzaStore nyStore = new NYPizzaStore();
+        Pizza pizza = nyStore.createPizza("cheese");
+
+    }
+}
---
+++ FactoryPattern/src/NYStyleVeggiePizza.java
@@ -0,0 +1,8 @@
+public class NYStyleVeggiePizza extends Pizza{
+    public NYStyleVeggiePizza() {
+        name = "NY Style Veggie Pizza";
+        dough = "Thin Crust Dough";
+        sauce = "No Sauce";
+        toppings.add("I don't know what veggie is LOL!");
+    }
+}
